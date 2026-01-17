1b1dafe81f4c514c7b24e2e9aae5ab794793a1a9
Shrishjay Acharya <shrishjay2004@protonmail.com>
2026-01-17 21:35:51 +0530

    completed the connection between backend and ML model

--- ML_pipeline/src/model.py
+++ ML_pipeline/src/model.py
@@ -5,6 +5,10 @@ from sklearn.ensemble import RandomForestRegressor
 import pickle
 
 
+file_path = "/home/shrishjay/projects/Crop_Yield_Prediction/ML_pipeline/data/crop_production.csv"
+df = pd.read_csv(file_path)
+
+
 def training(df):
     x = df.drop(["Yield_ton_per_hec"], axis=1)
     y = df["Yield_ton_per_hec"]
@@ -12,8 +16,10 @@ def training(df):
         x, y, test_size=0.20, random_state=42
     )
     regressor = RandomForestRegressor(random_state=0, oob_score=True)
+    print(x_train)
+    print(x_test)
     model = regressor.fit(x_train, y_train)
-    model_pkl_file = "model.pkl"
+    model_pkl_file = "/home/shrishjay/projects/Crop_Yield_Prediction/model/model.pkl"
     with open(model_pkl_file, "wb") as file:
         pickle.dump(model, file)
     print(model.score(x_test, y_test))
--- src/data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv(
-    "/home/shrishjay/projects/Crop-Yeild-Prediction/data/crop_production.csv"
-)
-print(df.head())
-print(df.columns)
